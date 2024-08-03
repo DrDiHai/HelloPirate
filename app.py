69305211e8ec1201f02fe124fbab4cd7c2941ce4
DrDiHai <sven.kindlimann@gmail.com>
2024-08-03 19:18:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, send_from_directory
 import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-# Define the root route
-@app.route('/')
-def serve_index():
-    # Get the directory where the script is located
-    script_dir = os.path.dirname(os.path.realpath(__file__))
 
-    # Serve the HTML file from the same directory
-    return send_from_directory(script_dir, 'index.html')
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    # Start the Flask web server
-    app.run()
+   app.run()
